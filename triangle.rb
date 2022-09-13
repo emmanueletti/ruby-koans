e36fbe3bd7aa7a95c6a3835ecc8e857634eeaa11
Emmanuel Etti <emmanueletti@gmail.com>
2022-09-12 19:12:49 -0700

    saving progress

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,38 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  validate_args(a, b, c)
+
+  unique_values = [a, b, c].uniq
+
+  case unique_values.length
+  when 1
+    :equilateral
+  when 2
+    :isosceles
+  else
+    :scalene
+  end
+end
+
+def validate_args(*args)
+
+  invalid_values = args.select { |value| value <= 0 }
+
+
+  if invalid_values.size < 0
+    raise TriangleError
+  end
+
+  side_1, side_2, side_3 = args
+
+  combination_1 = (side_1 + side_2) <= side_3
+  combination_2 = (side_1 + side_3) <= side_2
+  combination_3 = (side_2 + side_3) <= side_1
+
+  if combination_1 || combination_2 || combination_3
+    raise TriangleError
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
